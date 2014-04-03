04846cca09a30df6f94d47ea1e45dc82646a07e0
amieding <amie.ding@vocalocity.com>
2014-04-02 23:05:11 -0400

    git init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BankApp::Application.config.secret_token = '9cda04bfd36eec3728b48fb823a6d95a7afdb8bb7dcbb41105e6173b973060e6b397e7defe1a959b876403c37e79a5857c1ec1b7c631f2ba5e8e8c93cb5f017d'
